2b7fd346a7dbd8540fd0d89fd3e441bc479b5c5e
Peter Lane <lane@uchicago.edu>
2016-11-08 14:31:19 -0600

    create limited sudo config for waggle to start test service

--- scripts/configure-system.sh
+++ scripts/configure-system.sh
@@ -13,6 +13,8 @@ sed -i 's/^#ListenAddress 0.0.0.0$/ListenAddress 10.31.81.51/' /etc/ssh/sshd_con
 # /etc/network/interfaces, so just replace it with what we want
 cp ./etc/interfaces /etc/network/interfaces
 
+cp ./etc/waggle-test-service /etc/sudoers.d/
+
 # add AoT guest node cert
 echo "from=\"10.31.81.0/24\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4ohQv1Qksg2sLIqpvjJuZEsIkeLfbPusEaJQerRCqI71g8hwBkED3BBv5FehLcezTg+cFJFhf2vBGV5SbV0NzbouIM+n0lAr6+Ei/XYjO0B1juDm6cUmloD4HSzQWv+cSyNmb7aXjup7V0GP1DZH3zlmvwguhMUTDrWxQxDpoV28m72aZ4qPH7VmQIeN/JG3BF9b9F8P4myOPGuk5XTjY1rVG+1Tm2mxw0L3WuL6w3DsiUrvlXsGE72KcyFBDiFqOHIdnIYWXDLZz61KXctVLPVLMevwU0YyWg70F9pb0d2LZt7Ztp9GxXBRj5WnU9IClaRh58RsYGhPjdfGuoC3P AoT_guest_node_key" >> /home/waggle/.ssh/authorized_keys
 echo >> /home/waggle/.ssh/authorized_keys
