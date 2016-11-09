3bbd3b027c6bec5373ee69ef4252ead38a844633
Peter Lane <lane@uchicago.edu>
2016-11-09 09:22:07 -0600

    Turn test service start script into a run script by waiting for the service to stop before returning.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,4 +4,11 @@ if [[ ! -e /home/waggle/continue_test && ! -e /home/waggle/finish_test ]]; then
   touch /home/waggle/start_test
 fi
 sudo /bin/systemctl start waggle-test.service
-sleep 5
+
+sleep 2
+
+systemctl status waggle-test --no-pager
+while [ $? -eq 0 ]; do
+  sleep 2
+  systemctl status waggle-test --no-pager
+done
