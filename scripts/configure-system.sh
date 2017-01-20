d2d10bad08fff7a46965f90a929157fbef737c31
Peter Lane <lane@uchicago.edu>
2017-01-20 10:35:08 -0600

    moved interfaces file to reflect actual system location

--- scripts/configure-system.sh
+++ scripts/configure-system.sh
@@ -11,9 +11,9 @@ sed -i 's/^#ListenAddress 0.0.0.0$/ListenAddress 10.31.81.51/' /etc/ssh/sshd_con
 
 # NetworkManager will try to manage any interfaces *not* listed in
 # /etc/network/interfaces, so just replace it with what we want
-cp ./etc/interfaces /etc/network/interfaces
+cp ./etc/network/interfaces /etc/network/interfaces
 
-cp ./etc/waggle-test-service /etc/sudoers.d/
+cp ./etc/sudoers.d/waggle-test-service /etc/sudoers.d/
 
 # add AoT guest node cert to authorized_keys files
 echo "from=\"10.31.81.0/24\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4ohQv1Qksg2sLIqpvjJuZEsIkeLfbPusEaJQerRCqI71g8hwBkED3BBv5FehLcezTg+cFJFhf2vBGV5SbV0NzbouIM+n0lAr6+Ei/XYjO0B1juDm6cUmloD4HSzQWv+cSyNmb7aXjup7V0GP1DZH3zlmvwguhMUTDrWxQxDpoV28m72aZ4qPH7VmQIeN/JG3BF9b9F8P4myOPGuk5XTjY1rVG+1Tm2mxw0L3WuL6w3DsiUrvlXsGE72KcyFBDiFqOHIdnIYWXDLZz61KXctVLPVLMevwU0YyWg70F9pb0d2LZt7Ztp9GxXBRj5WnU9IClaRh58RsYGhPjdfGuoC3P AoT_guest_node_key" >> /home/waggle/.ssh/authorized_keys
