c607ff8fe76e9a5a39bdb2bbb479ccc158422824
Peter Lane <lane@uchicago.edu>
2017-01-21 23:22:58 -0600

    don't blank root's authorized_keys file either, dumby

--- scripts/configure-system.sh
+++ scripts/configure-system.sh
@@ -22,5 +22,4 @@ cp ${script_dir}/../etc/sudoers.d/* /etc/sudoers.d/
 mkdir -p /root/.ssh
 chmod 744 /root/.ssh
 echo "from=\"10.31.81.0/24\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4ohQv1Qksg2sLIqpvjJuZEsIkeLfbPusEaJQerRCqI71g8hwBkED3BBv5FehLcezTg+cFJFhf2vBGV5SbV0NzbouIM+n0lAr6+Ei/XYjO0B1juDm6cUmloD4HSzQWv+cSyNmb7aXjup7V0GP1DZH3zlmvwguhMUTDrWxQxDpoV28m72aZ4qPH7VmQIeN/JG3BF9b9F8P4myOPGuk5XTjY1rVG+1Tm2mxw0L3WuL6w3DsiUrvlXsGE72KcyFBDiFqOHIdnIYWXDLZz61KXctVLPVLMevwU0YyWg70F9pb0d2LZt7Ztp9GxXBRj5WnU9IClaRh58RsYGhPjdfGuoC3P AoT_guest_node_key" > /root/.ssh/authorized_keys
-echo > /root/.ssh/authorized_keys
 
