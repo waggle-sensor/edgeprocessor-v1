61fa03ca9db3b6e1a998b55619c0368b32c6f99b
Peter Lane <lane@uchicago.edu>
2017-03-01 16:18:22 -0600

    verbose EP date update

--- scripts/sync_date.sh
+++ scripts/sync_date.sh
@@ -5,5 +5,5 @@ date=$1
 date -s@${date}
 
 # Sync the system time with the hardware clock
-echo "Syncing the hardware clock with the system date/time..."
+echo "Syncing the Edge Processor hardware clock with the system date/time..."
 hwclock -w
