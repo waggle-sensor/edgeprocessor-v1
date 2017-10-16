2ef3293e5efd0f0027a0ce68f51a2d64b907a8fd
Yongho Kim <ironyonghokim@gmail.com>
2017-10-16 16:22:09 -0500

    Fixed syntax error

--- image/producer/image_producer.py
+++ image/producer/image_producer.py
@@ -37,7 +37,7 @@ def main():
   connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
   channel = connection.channel()
   # channel.exchange_delete(exchange='image_pipeline')
-  channel.exchange_declare(exchange='image_pipeline', type='direct')
+  channel.exchange_declare(exchange='image_pipeline', exchange_type='direct')
 
   try:
     while True:
