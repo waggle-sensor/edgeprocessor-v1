4f0570376b250c27c6f99130fd0d20c3e691bf3e
Yongho Kim <ironyonghokim@gmail.com>
2017-11-28 15:54:20 -0600

    Added copyright to exif

--- image/exporter/image_exporter.py
+++ image/exporter/image_exporter.py
@@ -20,6 +20,7 @@ null_exif = {
   '1st': {},
   'thumbnail': None
 }
+copy_right = 'Copyright to Waggle (http://wa8.gl) and Array of Things (). Do not use without explicit permission'
 
 
 def generate_meta_data(meta_data, results):
@@ -37,6 +38,7 @@ def generate_meta_data(meta_data, results):
     oth[piexif.ImageIFD.Software] = meta_data['producer']
   if 'datetime' in meta_data:
     oth[piexif.ImageIFD.DateTime] = meta_data['datetime']
+  oth[piexif.ImageIFD.Copyright] = copy_right
   exif_dict['0th'] = oth
 
   exif = exif_dict['Exif']
